06390b6afa50c259d6c5bbfd0f7e70b3af74475b
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-23 15:29:11 +0000

    lab2

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -13,3 +13,62 @@
 # Elsif Conditional Logic
 
 # Combining Expressions
+
+# #Prueba 1
+
+# if 1 + 1 ==2
+#     puts "this is true"
+# end
+
+# if 1+1 ==3
+#     puts "you should never see this"
+#     puts "no really"
+# end 
+
+
+# #analogy for user name and password 
+# #Input 
+# favorite_food="tacos"
+# lunch = "tacos"
+
+# #output
+# if lunch ==="favorite_food"
+#     puts "yay"
+# end
+
+# #user and psw
+# #input
+# real_psw="tacos"
+# user_entered_psw="secret"
+
+# #output
+# if user_entered_psw==real_psw
+#     puts "You are in!"
+# else
+# puts "oops!"
+# end
+
+# our_team=5
+# other_team=2
+
+# if our_team>other_team
+#     puts "You win"
+# elsif   our_team==other_team
+# puts "you tied"
+# else
+#     puts "You lose"
+# end
+
+#Combined expressions
+
+    temperature = 32
+    precip = 100
+
+    if temperature > 60 && precip ==0
+        puts "Nice"
+    elsif temperature >60 && precip >0
+    puts "sad"
+    else
+        puts "gross"
+    end
+
