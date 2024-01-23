fe02e95c394988834031798995f78c16ef06b3fb
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-23 16:02:09 +0000

    lab2

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -10,3 +10,41 @@
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+#Create a list of my fav food
+fav_food= [
+"tacos",
+"pizza",
+"lomo saltado",
+3,
+false,
+nil
+]
+
+#add new item in the list 
+fav_food.push << "Dumplings"
+
+#puts fav_food
+
+# I want to select item of a list
+puts fav_food[0]
+
+#Combine two lists = combine below in the list
+bens_fav_food = ["kale", "sticks","berries"]
+
+colective_food=fav_food + bens_fav_food
+puts colective_food
+
+# #Shopping list = 
+shopping_list = [fav_food, bens_fav_food]
+
+# #i want only one element (one table)
+puts shopping_list [1]
+
+# # #i want the specific 1st item of the second list
+puts shopping_list[1][0]
+
+#number of items in our collective list
+x = "Number of items on list: #{fav_food.size}"
+
+puts x
